51c81e06682ba43a92a18f25a8c8d3436929a0ab
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-06 14:59:08 -0500

    Moved layout to part of the static_elements folder

--- pbi_core/__init__.py
+++ pbi_core/__init__.py
@@ -1,4 +1,5 @@
-from . import layout, ssas
+from . import ssas
+from .static_elements import layout
 
 __all__ = ["layout", "ssas"]
 __version__ = "0.1.0"
---
+++ pbi_core/static_elements/__init__.py
@@ -0,0 +1,23 @@
+from .layout import (
+    Layout,
+    LayoutNode,
+    ResourcePackage,
+    ResourcePackageDetails,
+    ResourcePackageDetailsType,
+    ResourcePackageItem,
+    ResourcePackageItemType,
+    Section,
+    load_pbix,
+)
+
+__all__ = [
+    "Section",
+    "Layout",
+    "load_pbix",
+    "LayoutNode",
+    "ResourcePackage",
+    "ResourcePackageDetails",
+    "ResourcePackageDetailsType",
+    "ResourcePackageItem",
+    "ResourcePackageItemType",
+]
