d0adaf3d2c99d401795ea96be70f13d8bb7548ed
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-06 14:59:04 -0500

    Added parents to resource packages

--- pbi_core/layout/resource_package.py
+++ pbi_core/layout/resource_package.py
@@ -1,10 +1,11 @@
+import inspect
 from enum import IntEnum
 from typing import TYPE_CHECKING, Optional
 
 from ._base_node import LayoutNode
 
 if TYPE_CHECKING:
-    pass
+    from .layout import Layout
 
 
 class ResourcePackageItemType(IntEnum):
@@ -19,6 +20,7 @@ class ResourcePackageItemType(IntEnum):
 
 
 class ResourcePackageItem(LayoutNode):
+    parent: "ResourcePackageDetails"
     name: Optional[str] = None
     path: str
     type: ResourcePackageItemType
@@ -34,6 +36,7 @@ class ResourcePackageDetailsType(IntEnum):
 
 
 class ResourcePackageDetails(LayoutNode):
+    parent: "ResourcePackage"
     disabled: bool
     items: list[ResourcePackageItem] = []
     type: ResourcePackageDetailsType
@@ -42,4 +45,17 @@ class ResourcePackageDetails(LayoutNode):
 
 
 class ResourcePackage(LayoutNode):
+    parent: "Layout"
     resourcePackage: ResourcePackageDetails
+
+
+"""
+woo boy. Why is this code here? Well, we want a parent attribute on the objects to make user navigation easier
+This has to be a non-private attribute due to a bug in pydantic right now.
+We know we'll add the parent attribute after pydantic does it's work, but we want mypy to think the parent is
+always there. Therefore we check all objects with parents and make the default None so the "is_required" becomes False
+https://github.com/pydantic/pydantic/blob/a764871df98c8932e9b7bc10d861053d110a99e4/pydantic/fields.py#L572
+"""
+for name, obj in list(globals().items()):
+    if inspect.isclass(obj) and issubclass(obj, LayoutNode) and "parent" in obj.model_fields:
+        obj.model_fields["parent"].default = None
