2f501007f60d4236118273875b2442f3ce58ed69
gitea <a@a.com>
2025-09-12 15:24:59 -0500

    Reorganized section to be easier to work on

--- pbi_core/static_files/layout/visuals/scatter_chart.py
+++ pbi_core/static_files/layout/visuals/scatter_chart.py
@@ -3,7 +3,7 @@ from pydantic import Field
 from pbi_core.static_files.layout._base_node import LayoutNode
 from pbi_core.static_files.layout.selector import Selector
 
-from .base import BaseVisual, ColumnProperty
+from .base import BaseVisual
 from .properties.base import Expression
 
 
